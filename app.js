63375ec29bb691fe1fb3ae3d2be4dfcd637f3c08
Nyein Soe <kaiyeolbaek8422@gmail.com>
2026-09-17 23:23:09 +0630

    feat: result = num1 + num2 and clear button

--- app.js
+++ app.js
@@ -11,13 +11,52 @@
 //     })
 // })
 
-let key = document.querySelectorAll('.key');
+let keys = document.querySelectorAll('.key');
 let display = document.getElementById('display');
+let a = ""; let b = ""; let result = 0; let o;
 
-key.forEach(keys =>{
-    keys.addEventListener('click' , (event)=>{
-        let value = event.currentTarget.innerHTML;
-        console.log(value);
-        display.innerHTML = value;
-    })
-})
+keys.forEach(key =>{
+    console.log(key.id);
+    if(key.classList.contains('key--num')){
+        key.addEventListener('click' , (event) =>{
+            let value = event.currentTarget.innerHTML;
+            // console.log(typeof(value));
+            display.innerHTML += value;
+            if(!o){
+                a += value;
+            }
+            else{
+                b += value;
+                console.log(b);
+                
+            }
+        })
+    } 
+    else if(key.classList.contains('key--op')){
+        key.addEventListener('click', (event) =>{
+            let value = event.currentTarget.innerHTML;
+            display.innerHTML += value;
+            o = value;
+        })
+    }
+    else if(key.classList.contains('key--eq')){
+        key.addEventListener('click', (event) =>{
+            result = Number(a) + Number(b);
+            display.innerHTML = result;
+        })
+    } 
+    else if(key.id === 'key-clear'){
+        key.addEventListener('click', (event) =>{
+            display.innerHTML = "";
+        })
+    }
+}
+
+    // console.log(key.classList.contains('key--num'));
+    // if(key.className == key--num)
+    // key.addEventListener('click' , (event)=>{
+    //     let value = event.currentTarget.innerHTML;
+    //     console.log(value);
+    //     display.innerHTML += value;
+    // })
+)
